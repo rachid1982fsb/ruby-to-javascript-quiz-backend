201126f42640f060f2cf5b13c617fd907e0498f1
Rachid Rakaa <rachid1982fsb@gmail.com>
2020-01-17 14:18:52 -0800

    Add validation for Correct Resonse so the each Answer Method should be unique for each user

--- app/models/correct_response.rb
+++ app/models/correct_response.rb
@@ -1,3 +1,5 @@
 class CorrectResponse < ApplicationRecord
     belongs_to :user
+    validates :source_id, uniqueness: { scope: :user_id,
+    message: "Answer Method should be unique for each user" }
 end
